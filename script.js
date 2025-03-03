f57cf02bdceb21bafe72255276fefd04921b97d0
sandra8891 <sandraks8891@gmail.com>
2025-03-03 16:13:58 +0530

    march03

---
+++ script.js
@@ -0,0 +1,70 @@
+//var
+//console.log(x)
+//var x=20
+//console.log(x)
+
+// console.log(x)
+// var x=20
+// var x=25
+// x=24
+// console.log(x)
+
+//primitve datatypes
+//let a=10
+//let a=10.5
+//let a="as"
+//let a=true
+//let a=false
+//let a
+//let a=null
+// console.log(a)
+// console.log(typeof a)
+// let sym=symbol('unique')
+// console.log(sym)
+// console.log(typeof sym)
+
+//non-primitive
+//array
+// let a=[1,2,3,4]
+// console.log(a)
+// console.log(typeof a)
+
+// let a=[1,2,3,'assd',[1,2,3]]
+// console.log(a)
+// console.log(typeof a)
+
+//object
+// let data ={name:'anu'}
+// console.log(data)
+// console.log(typeof a)
+
+//operators
+//arthimetic
+// console.log(10+30)
+// console.log(10-30)
+// console.log(10*30)
+// console.log(10/30)
+// console.log(10%30)
+// console.log(10**30)
+
+//assignment
+// let a=10
+// a+=10
+// console.log(a)
+
+//comparison
+// a=10
+// b=10
+// console.log(a===b)
+// console.log(a!=b)
+// console.log(a!==b)
+
+//logical
+// console.log(10==10 && 10>5)
+// console.log(10==10 || 10>5)
+// console.log(!10==10)
+
+//ternery
+a=10
+b=50
+a>b? console.log('a'): console.log(b)
