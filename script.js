ce668db7d80354a91172ba8b84a9264356653c5b
sandra8891 <sandraks8891@gmail.com>
2025-03-04 16:07:03 +0530

    march04

--- script.js
+++ script.js
@@ -65,6 +65,117 @@
 // console.log(!10==10)
 
 //ternery
+// a=10
+// b=50
+// a>b? console.log('a'): console.log(b)
+
+//conditional statements
+// a=10000
+// b=6
+// if(a>5){
+//     console.log(a*5/100)
+// }
+// else{
+//     console.log('no bonus')
+// }
+
+
+
+// a=350
+// if(a<=100){
+//     console.log('no charge')
+// }
+// else if(a<=200){
+//     console.log((a-100)*5)
+// }
+// else{
+//     console.log((100*5)+(a-200)*10)
+// }
+
+
+// a=1
+// if(a==1){
+//     console.log('sunday')
+// }
+// else if(a==2){
+//     console.log('monday')
+// }
+// else if(a==3){
+//     console.log('tuesday')
+// }
+// else if(a==4){
+//     console.log('wednesday')
+// }
+// else if(a==5){
+//     console.log('thursday')
+// }
+// else if(a==6){
+//     console.log('friday')
+// }
+// else if(a==7){
+//     console.log('saturday')
+// }
+// else{
+//     console.log('not a day')
+// }
+
+
+
+// a='jaipur'
+
+// if(a=='delhi'){
+//     console.log('redfort')
+// }
+// else if(a=='agra'){
+//     console.log('tajmahal')
+// }
+// else if(a=='jaipur'){
+//     console.log('jalmahal')
+// }
+// else{
+//     console.log('not found')
+// }
+
+
+
+// a=13
+// b=a%10
+// if(b%3==0){
+//     console.log('divisible by 3')
+// }
+// else{
+//     console.log('not divisible by 3')
+// }
+
+
+// a=150000
+// if(a>100000){
+//     console.log(a*15/100+a)
+// }
+// else if(a>50000 && a<=100000){
+//     console.log(a*10/100+a)
+// }
+// else{
+//     console.log(a<=50000+a)
+// }
+
+
 a=10
-b=50
-a>b? console.log('a'): console.log(b)
+b=40
+c=30
+if(a>=b){
+    if(a>=c){
+        console.log('a is greater')
+    }
+    else{
+        console.log('b is greater')
+    }
+}
+else{
+    if(b>=c){
+        console.log('b is greater')
+    }
+    else{
+        console.log('c is greater')
+    }
+}
