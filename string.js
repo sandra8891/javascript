e20a2ed0766b77adab11fcfb654a16a5709d6308
sandra8891 <sandraks8891@gmail.com>
2025-03-12 15:59:21 +0530

    march12

--- string.js
+++ string.js
@@ -4,11 +4,71 @@
 //         document.getElementById("result").innerHTML=a.value.split("").reverse().join("")
 //     }
 
-function getdata(){
-    let a=[]
-    let b=document.getElementById("input")
-    let c=document.getElementById("input1")
-    let d=document.getElementById("input2")
-    
+// function getdata(){
+//     let b=document.getElementById("input").value
+//     let c=document.getElementById("input1").value
+//     let d=document.getElementById("input2").value
+//     let a=[]
+//     a.push(b,c,d)
+//     console.log(a)    
 
-}
+// }
+
+const data=[]
+// function getdata(){
+//     let no=document.getElementById("no").value
+//     let name=document.getElementById("name").value
+//     let age=document.getElementById("age").value
+//     data.push({no:no,name:name,age:age})
+//     console.log(data)    
+//     display()
+// }
+
+// function getdata(){
+//     let no=document.getElementById("no").value
+//     let name=document.getElementById("name").value
+//     let age=document.getElementById("age").value
+//     data.push({no:no,name:name,age:age})
+//     document.getElementById("no").value=''
+//     document.getElementById("name").value=''
+//     document.getElementById("age").value=''
+//     console.log(data)   
+//     display()
+// }
+
+// function getdata(){
+//     let no=document.getElementById("no")
+//     let name=document.getElementById("name")
+//     let age=document.getElementById("age")
+//     data.push({no:no.value,name:name.value,age:age.value})
+//     no.value=name.value=age.value=' '
+//     console.log(data)    
+//     display()
+// }
+
+
+
+
+
+// function display(){
+//     let tbody=document.getElementById("tbody")
+//     tbody.innerHTML=``
+//     for(user of data){
+//         const tr=document.createElement("tr")
+//         tr.innerHTML=`
+//         <td>${user.no}</td>
+//         <td>${user.name}</td>
+//         <td>${user.age}</td>
+//         `
+//         tbody.appendChild(tr)
+//     }
+// }
+// display()
+
+
+
+// let a=document.querySelector("p")
+// let a=document.querySelectorAll("p")
+// let a=document.querySelector("#sample")
+// let a=document.querySelector(".s1")
+// console.log(a)
