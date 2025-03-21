2430258881e89d4010ad94e015596a0853a6cb9b
sandra8891 <sandraks8891@gmail.com>
2025-03-21 17:33:31 +0530

    api

--- api.js
+++ api.js
@@ -79,3 +79,98 @@
     
 // })
 
+// const data=new Promise((resolve,reject)=>{
+//     if(true){
+//         resolve('resolved')
+//     }else{
+//         reject('rejected')
+//     }
+// })
+// console.log(data);
+// data.then((message)=>{
+//     console.log(message);
+    
+// }).catch((message)=>{
+//     console.group(message)
+// }).finally(()=>{
+//     console.log('finally');
+    
+// })
+
+
+
+//api get cheyan
+
+// async function fetData() {
+//     try{
+//         const response=await fetch("https://dummyjson.com/quotes")
+//         const products=await response.json()
+//         console.log('products',products);
+        
+//     }catch(error){
+
+//     }
+// }
+// fetData()
+
+
+//html page display cheyan
+
+// async function fetData() {
+//     try{
+//         const response=await fetch("https://dummyjson.com/products")
+//         const products=await response.json()
+
+
+//         const data=products.products
+//         const div=document.querySelector(".div")
+
+//         data.forEach(item => {
+//             console.log(item.title);
+//             const h1=document.createElement("h1")
+//             h1.innerHTML=item.title
+//             div.appendChild(h1)
+//         })
+//     }catch(error){
+
+//     }
+    
+// }
+// fetData()
+
+
+
+async function fetData() {
+        try{
+            const response=await fetch("https://dummyjson.com/products")
+            const products=await response.json()
+    
+    
+            const data=products.products
+            const div=document.querySelector(".div")
+    
+            data.forEach(item => {
+                console.log(item);
+                div.innerHTML+=`
+            <div class="col-lg-4">
+                <div class="card" style="width:100%">
+                     <img class="card-img-top" src="${item.thumbnail}" alt="Card image" style="width:100%">
+                    <div class="card-body">
+                        <h4 class="card-title">${item.title}</h4>
+                        <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                         <a href="#" class="btn btn-primary">See Profile</a>
+                    </div>
+                </div>
+            </div>
+
+
+        `
+                
+                
+            })
+        }catch(error){
+    
+        }
+        
+    }
+    fetData()
